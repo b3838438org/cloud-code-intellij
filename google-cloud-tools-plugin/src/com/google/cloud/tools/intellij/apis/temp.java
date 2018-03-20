ce4cf170b675650d4ea60fef525e0d229571fc17
Nabila Bashir-Bello <nbashirbello@google.com>
2018-03-20 14:58:01 -0400

    Added replacement dialog for Service Account Created

---
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/apis/temp.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.intellij.apis;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JTable;
+
+/**
+ * Created by nbashirbello on 3/20/18.
+ */
+public class temp {
+
+  private JLabel yourServiceAccountKeyLabel;
+  private JLabel envVarInfoText;
+  private JTable table1;
+  private JButton printButton;
+  private JList list1;
+  private JButton button2;
+  private JButton button5;
+  private JLabel downloadPathLabel;
+}
